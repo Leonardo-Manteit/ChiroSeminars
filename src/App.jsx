570d3fd0b7c1d362586959f7485453bb33cc2dfd
Alexander <alexanderkkampfer@gmail.com>
2024-10-01 15:18:02 +1000

    converted to react-browser-dom layout, begun work on components, basic homepage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
-import Nav from './components/Nav/Nav'
-import Footer from './components/footer/Footer'
-import CreateEvents from './components/createEvent/CreateEvents'
-import Featured from './components/featured/Featured'
-import GetUpdates from './components/getUpdates/GetUpdates'
-import TopicList from './components/TopicList/TopicList'
-import Testimonials from './components/Testimonials/Testimonials'
-import SearchBar from './components/SearchBar/SearchBar'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Root from './components/Root/Root'
+// import Nav from './components/Nav/Nav';
+// import Footer from './components/Footer/Footer';
+import CreateEvents from './components/CreateEvent/CreateEvents';
+import Featured from './components/Featured/Featured';
+import GetUpdates from './components/GetUpdates/GetUpdates';
+import TopicList from './components/TopicList/TopicList';
+import Testimonials from './components/Testimonials/Testimonials';
+// import SearchBar from './components/SearchBar/SearchBar';
+import './App.css';
+import Seminars from './components/Seminars/Seminars';
 
 function App() {
-
   return (
-    <>
-      <Nav />
-      <SearchBar />
-      <Featured />
-      <TopicList />
-      <CreateEvents />
-      <Testimonials />
-      <GetUpdates />
-      <Footer />
-    </>
-  )
+    
+    <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/featured" element={<Featured />} />
+        <Route path="/seminars" element={<Seminars />} />
+        <Route path="/topics" element={<TopicList />} />
+        <Route path="/create-event" element={<CreateEvents />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/updates" element={<GetUpdates />} />
+      </Routes>
+          
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import Nav from '../Nav/Nav';
+import SearchBar from '../SearchBar/SearchBar';
+import Footer from '../Footer/Footer'
+
+export default function Root() {
+    return (
+    <section className="root">
+        
+        <Nav />
+        <SearchBar />
+        <Footer />
+    </section>
+    )
+}
