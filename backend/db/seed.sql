fbc04e18de92b4473cb5bda73c16b9f2ed6ff68b
Jesus130247 <leonardo.monty@hotmail.com>
2024-10-18 12:38:51 +1100

    seeded psql 2 seminars, changed a route

---
+++ backend/db/seed.sql
@@ -0,0 +1,40 @@
+INSERT INTO seminars   
+(title, organizer, date, location, description, price, contact, featured) VALUES 
+('Risk Management Videos', 'ACA', '1 May, 2023 @ 8:00 am AEST — 30 June, 2025 @ 11:00 pm AEST', 'Melbourne, VIC',
+'<h1>A National Initiative on Risk Management Standardisation 2019</h1><p>In order to maximise the learning opportunities afforded by leading Risk Management expert, Dr John Kelly, ACA are offering chiropractors the opportunity to purchase the 2019 Risk Management Roadshow recording.</p>
+<p>Dr Kelly has reviewed the changing face of litigation over the past 33 years, providing a synopsis of the “then and now” of the issues faced by chiropractors in clinical practice.</p>
+<p>Take the opportunity to learn valuable, first-hand lessons from real Australian cases. The Roadshow covers:</p>
+<ul><li>histories which should have triggered caution;</li><li>differential diagnostic options considered;</li><li>informed consent procedures;</li>
+        <li>technique selection issues;</li><li>monitoring of patient progress; and</li><li>the issue of when to refer – or not?</li></ul>
+<h2>CPD</h2><p>The content in these videos may be incorporated as part of your annual learning plan.</p>
+<p>Once your payment has been processed you will receive a link to the videos.</p><p>After watching the videos, complete the online questionnaire to receive your completion statement.</p>'
+, '$165.00 - $247.50', 'NA', 0),
+('AICE Sports & Exercise Grand Rounds Webinar', 'AICE', '23 October @ 8:30 pm - 9:30 pm AEDT', 'Melbourne, VIC', 
+'<h1>AICE Sports & Exercise Webinar</h1>
+
+    <h2>Grand Rounds presented by Alex Fielding, Dr David Hendrey & Dr Theunis Nieuwoudt</h2>
+
+    <p>Our panelists will feature three interesting sports chiropractic case studies. Filled with clinical pearls, the speakers will discuss examination, diagnosis & management of three different conditions, with question time after each session to ask your burning questions.</p>
+
+    <h3>Panelists & Case Studies</h3>
+
+    <ul>
+        <li><strong>Alex Fielding</strong> – Management of a 5th metatarsal fracture in an Olympic level High Jumper<br>
+            Alex will be discussing a case of an Olympic Level high jumper and the management and return to performance at a world championship level.
+        </li>
+        <li><strong>Dr David Hendrey</strong> – Taking Subjective Testing to Objective: 3 x Concussion Case Studies<br>
+            David will be discussing 2 acute concussions (1 with ADHD) and 1 post-concussive syndrome case.
+        </li>
+        <li><strong>Dr Theunis Nieuwoudt</strong> – Use of Low Level Laser Therapy (LLLT) for Chronic Low Back Pain in a Youth Athlete<br>
+            Theunis will be discussing the use of Low Level Laser Therapy Treatment (LLLT) for chronic debilitating low back pain in a female youth elite-level basketball player.
+        </li>
+    </ul>
+
+    <h3>Event Details</h3>
+    <p><strong>Date:</strong> Wednesday 23 October 2024<br>
+       <strong>Time:</strong> 8:30pm to 9:30pm AEDT</p>', 
+'Free - $99.00', 'NA', 0);
+
+
+-- good website for ideas : https://www.chiropractors.org.au/event/risk-management-videos
+
