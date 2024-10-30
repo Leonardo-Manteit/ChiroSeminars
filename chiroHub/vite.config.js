910c4d5ccc61079520c7d11bcf9d10032940573e
Jesus130247 <leonardo.monty@hotmail.com>
2024-10-30 17:58:29 +1100

    deployment ready?

--- chiroHub/vite.config.js
+++ chiroHub/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/ChiroSeminars/', // Use the path relevant to your deployment
   plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
     } 
   }
-})
+});
