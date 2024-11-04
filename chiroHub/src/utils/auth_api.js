5ebea0d2dae241b288e3496a027919a45eb19d65
Leonardo M <150982381+Jesus130247@users.noreply.github.com>
2024-11-04 13:33:22 +1100

    Update auth_api.js
    changed api - chiro

--- chiroHub/src/utils/auth_api.js
+++ chiroHub/src/utils/auth_api.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export async function login(userInfo) {
-    let res = await axios.post(`/api/login`, userInfo) 
+    let res = await axios.post(`/chiro/login`, userInfo) 
     console.log(res)
     return res.data.token
 }
 
 export async function signUp(userInfo) {
-    let res = await axios.post(`/api/signUp`, userInfo) 
+    let res = await axios.post(`/chiro/signUp`, userInfo) 
     console.log(res)
     return res.data.token
 }
