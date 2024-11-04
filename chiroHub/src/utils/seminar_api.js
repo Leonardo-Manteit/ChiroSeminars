1e8063f74554baaa12eab308ecab7a8eb9788380
Leonardo M <150982381+Jesus130247@users.noreply.github.com>
2024-11-04 13:33:47 +1100

    Update seminar_api.js
    changed api - chiro

--- chiroHub/src/utils/seminar_api.js
+++ chiroHub/src/utils/seminar_api.js
@@ -1,25 +1,25 @@
 import axios from 'axios'
 
 export async function getSeminars() {
-    let res = await axios.get(`/api/seminars/`)
+    let res = await axios.get(`/chiro/seminars/`)
     // console.log(res)
     return res.data
 }
 
 export async function getFeatured() {
-    let res = await axios.get(`/api/featured/`)
+    let res = await axios.get(`/chiro/featured/`)
     console.log(res)
     return res.data
 }
 
 export async function getSeminarById(seminar_id) {
-    let res = await axios.get(`/api/seminars/${seminar_id}`)
+    let res = await axios.get(`/chiro/seminars/${seminar_id}`)
     console.log(res)
     return res.data
 }
 
 export async function deleteSeminar(seminar_id) {
-    let res = await axios.delete(`/api/delete/${seminar_id}`)
+    let res = await axios.delete(`/chiro/delete/${seminar_id}`)
     console.log(res)
     return res.data
 }
