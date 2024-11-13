6fb2ede6ce5fb1ef2fff8cd8d15fae4c02209dbf
Jesus130247 <leonardo.monty@hotmail.com>
2024-11-13 17:38:48 +1100

    trying to get profile image to work

--- chiroHub/src/utils/image_upload.js
+++ chiroHub/src/utils/image_upload.js
@@ -1,8 +1,5 @@
-export async function uploadProfilePhoto(image) {
-    const formData = new FormData();
-    formData.append('profilePhoto', image);
-
-    const response = await fetch('https://chiroseminarhub-australia.onrender.com/chiro/user/uploadProfilePhoto', {
+export async function uploadProfilePhoto(formData) {
+    const response = await fetch('/chiro/user/uploadProfilePhoto', {
         method: 'POST',
         body: formData,
     });
